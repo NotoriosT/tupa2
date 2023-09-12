3e48a770443cc7c6be0f79a0034351fd97d24a9e
Tiago <tonon2002@gmail.com>
2023-09-12 16:24:05 -0300

    master

---
+++ src/cabecalho/cabecalho/Cabelhaco.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import { FiLogOut } from 'react-icons/fi';
+import './Cabecalho.css';
+
+const Cabecalho = () => {
+
+    const [classOn, setClassOn] = useState(false);
+
+    return (
+        <header>
+            <div className="container">
+                <img className="logo-cyan" src="/img/cabecalho/logo3.png" alt="logo Cyan"/>
+
+                <div className = { classOn ? 'menu-section on' : 'menu-section'} onClick={() => setClassOn(!classOn)}>
+                    <div className="menu-toggle">
+                        <div className="one"></div>
+                        <div className="two"></div>
+                        <div className="three"></div>
+                    </div>
+
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="/">HOME</a>
+                            </li>
+                            <li>
+                                <a href="/">MISSÂO</a>
+                            </li>
+                            <li>
+                                <a href="/">PRODUTOS</a>
+                            </li>
+                            <li>
+                                <a href="/">SOBRE NÓS</a>
+                            </li>
+                            <li>
+                                <a href="/">CONTATO</a>
+                            </li>
+
+                        </ul>
+                    </nav>
+
+                </div>
+            </div>
+
+        </header>
+
+    )
+}
+
+export default Cabecalho;
+
